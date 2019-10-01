1e26c5b70d63e67b4db2ac2f15b120194efd11d9
Jack Ross <43938405+JackRossProjects@users.noreply.github.com>
2019-09-30 19:42:27 -0600

    started user collection and added bio

--- scripts/auth.js
+++ scripts/auth.js
@@ -32,13 +32,17 @@ createForm.addEventListener('submit', (e) => {
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  
+
   // get user info
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user
+  // sign up the user & add firestore data
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      bio: signupForm['signup-bio'].value
+    });
+  }).then(() => {
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
@@ -57,7 +61,7 @@ logout.addEventListener('click', (e) => {
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  
+
   // get user info
   const email = loginForm['login-email'].value;
   const password = loginForm['login-password'].value;
